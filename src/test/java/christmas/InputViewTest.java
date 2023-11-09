c9cd1cca0d5ebb5c2391d3fefb5fc49029e314be
vega <vega99c@naver.com>
2023-11-09 21:38:51 +0900

    test(InputViewTest): 방문날짜 유효성 테스트 검사 작성

---
+++ src/test/java/christmas/InputViewTest.java
@@ -0,0 +1,25 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    @Test
+    void 날짜입력값_숫자확인() {
+        InputView inputView = new InputView();
+        String inputString = "32";
+
+        assertThat(inputView.validateIsInteger(inputString)).isEqualTo(32);
+    }
+
+    @Test
+    void 날짜입력값_범위확인() {
+        InputView inputView = new InputView();
+        int inputDate = 32;
+
+        assertThatThrownBy(() -> inputView.validateIsCorrectRange(inputDate))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
