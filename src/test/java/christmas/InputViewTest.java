e74fb107f942b481b01b50ade2d127a5cfb39306
vega <vega99c@naver.com>
2023-11-10 18:31:01 +0900

    test(InputViewTest): 입력한 주문양식의 유효성 테스트 추가

--- src/test/java/christmas/InputViewTest.java
+++ src/test/java/christmas/InputViewTest.java
@@ -25,9 +25,24 @@ public class InputViewTest {
 
     @Test
     void 입력된메뉴_존재여부확인() {
-        String menuName = "해산물파스타";
+        String orderInfo = "해산물파스타-1";
+        String menuName = orderInfo.split("-")[0];
 
         InputView inputView = new InputView();
         inputView.isExistMenu(menuName);
     }
+
+    @Test
+    void 입력된주문양식_유효성확인() {
+        String orderInfo = "해산물파스타-1a";
+        String orderInfo2 = "해산물파스타1a";
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.isValidForm(orderInfo))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> inputView.isValidForm(orderInfo2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
