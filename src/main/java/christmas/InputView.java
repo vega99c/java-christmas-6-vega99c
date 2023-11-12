9d8fc1e0bdf485394c78ae84ccb05bf0c35b200f
vega <vega99c@naver.com>
2023-11-13 01:22:27 +0900

    refactor: InputView에 있으면 안되는 기능들 이전
    - Restaurant클래스로 주문접수와 관련된 메서드들 이전

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,151 +1,25 @@
 package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Set;
 
 public class InputView {
     private final String askReservationDate = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)\n";
     private final String askReservationMenuOrder = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g 해산물파스타-2,제로콜라-1)\n";
-    private String errorMsg;
-    private final int IDX_MENU_NAME = 0;
-    private final int IDX_MENU_PRICE = 1;
-    private int totalQuantity = 0;
-    private Hashtable<String, Integer> orderHashTable;
-    private List<String> menuList;
 
     public InputView() {
-        menuList = new ArrayList<String>();
-        orderHashTable = new Hashtable<>();
     }
 
-    public int readDate() {
+    public String readDate() {
         System.out.print(askReservationDate);
         String inputDate = Console.readLine();
-        int readDate = 0;
-
-        try {
-            readDate = validateIsCorrectRange(validateIsInteger(inputDate));
-        } catch (IllegalArgumentException error) {
-            System.out.print(error.getMessage());
-            readDate();
-        }
-
-        totalQuantity = 0;
-        return readDate;
-    }
-
-    public int getTotalQuantity() {
-        return totalQuantity;
-    }
-
-    public void readMenu() {
-        System.out.print(askReservationMenuOrder);
-        String inputMenu = Console.readLine();
-        List<String> menuList = null;
-        menuList = new ArrayList<String>(List.of(inputMenu.split(",")));
-
-        try {
-            validateMenuOrder(menuList);
-        } catch (IllegalArgumentException error) {
-            System.out.print(error.getMessage());
-            initiateOrderInfo();
-            readMenu();
-        }
-    }
-
-    public void validateMenuOrder(List<String> orderInfos) {
-        for (String order : orderInfos) {
-            isValidForm(order);
-        }
-
-        validateDuplicatedMenu();
-        validateOnlyDrink();
-    }
-
-    public void validateDuplicatedMenu() {
-        Set<String> menuSet = new HashSet<>(menuList);
-
-        if (menuSet.size() != menuList.size()) {
-            menuList.clear();
-            throw new IllegalArgumentException(ErrorMessages.INCLUDE_DUPLICATED_MENU.getErrorMsg());
-        }
-    }
-
-    public void initiateOrderInfo() {
-        totalQuantity = 0;
-    }
-
-    public void isValidForm(String orderInfo) {
-        String menuName = "";
-        String quantity = "";
-
-        try {
-            menuName = orderInfo.split("-")[IDX_MENU_NAME];
-            quantity = orderInfo.split("-")[IDX_MENU_PRICE];
-        } catch (ArrayIndexOutOfBoundsException error) {
-            throw new IllegalArgumentException(ErrorMessages.INCORRECT_MENU_ORDER.getErrorMsg());
-        }
-
-        int quantityNumber = validateIsInteger(quantity);
-        isExistMenu(menuName);
-        validateTotalOrderQuantity();
-        validateDuplicatedMenu();
-        orderHashTable.put(menuName, quantityNumber);
-    }
-
-    public boolean validateOnlyDrink() {
-        List<String> drinkList = new ArrayList<>(List.of("제로콜라", "레드와인", "샴페인"));
-
-        for (String menu : menuList) {
-            if (!drinkList.contains(menu)) {
-                return false;
-            }
-        }
-        menuList.clear();
-        throw new IllegalArgumentException(ErrorMessages.NOT_ALLOWED_ONLY_DRINK.getErrorMsg());
-    }
-
-    public void validateTotalOrderQuantity() {
-        if ((getTotalQuantity() < 1) || (getTotalQuantity() > 20)) {
-            errorMsg = ErrorMessages.NOT_INCLUDE_ORDER_RANGE.getErrorMsg();
-            totalQuantity = 0;
-            throw new IllegalArgumentException(errorMsg);
-        }
-    }
-
-    public void isExistMenu(String menuName) {
-        Menu menu = Menu.ROOT;
-        menu.contains(menuName);
-        menuList.add(menuName);
-    }
-
-    public int validateIsInteger(String string) {
-        int inputDate = 0;
-        try {
-            inputDate = Integer.parseInt(string);
-            totalQuantity += inputDate;
-        } catch (NumberFormatException e) {
-            errorMsg = ErrorMessages.NOT_NUMBER.getErrorMsg();
-            throw new IllegalArgumentException(errorMsg);
-        }
-
-        if (inputDate == 0) {
-            throw new IllegalArgumentException(ErrorMessages.NOT_ALLOW_ZERO.getErrorMsg());
-        }
 
         return inputDate;
     }
 
-    public int validateIsCorrectRange(int date) {
-        if (!((date >= 1) && (date <= 31))) {
-            errorMsg = ErrorMessages.INCORRECT_RANGE.getErrorMsg();
-            throw new IllegalArgumentException(errorMsg);
-        }
+    public String readMenu() {
+        System.out.print(askReservationMenuOrder);
+        String inputMenu = Console.readLine();
 
-        return date;
+        return inputMenu;
     }
 }
---
+++ src/main/java/christmas/Restaurant.java
@@ -0,0 +1,147 @@
+package christmas;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+public class Restaurant {
+    private final int IDX_MENU_NAME = 0;
+    private final int IDX_MENU_PRICE = 1;
+    InputView inputView;
+    private Hashtable<String, Integer> orderHashTable;
+    private List<String> menuList;
+    private int totalQuantity = 0;
+    private String errorMsg;
+
+    public Restaurant() {
+        menuList = new ArrayList<String>();
+        orderHashTable = new Hashtable<>();
+        inputView = new InputView();
+    }
+
+    public void receiptStart() {
+        int readDate = 0;
+        String inputDate = inputView.readDate();
+
+        try {
+            readDate = validateIsCorrectRange(validateIsInteger(inputDate));
+        } catch (IllegalArgumentException error) {
+            System.out.print(error.getMessage());
+            inputView.readDate();
+        }
+
+        totalQuantity = 0;
+    }
+
+    public void menuOrderStart() {
+        String inputMenu = inputView.readMenu();
+        List<String> menuList = null;
+        menuList = new ArrayList<String>(List.of(inputMenu.split(",")));
+
+        try {
+            validateMenuOrder(menuList);
+        } catch (IllegalArgumentException error) {
+            System.out.print(error.getMessage());
+            initiateOrderInfo();
+            inputView.readMenu();
+        }
+    }
+
+    public void isValidForm(String orderInfo) {
+        String menuName = "";
+        String quantity = "";
+
+        try {
+            menuName = orderInfo.split("-")[IDX_MENU_NAME];
+            quantity = orderInfo.split("-")[IDX_MENU_PRICE];
+        } catch (ArrayIndexOutOfBoundsException error) {
+            throw new IllegalArgumentException(ErrorMessages.INCORRECT_MENU_ORDER.getErrorMsg());
+        }
+
+        int quantityNumber = validateIsInteger(quantity);
+        isExistMenu(menuName);
+        validateTotalOrderQuantity();
+        validateDuplicatedMenu();
+        orderHashTable.put(menuName, quantityNumber);
+    }
+
+    public boolean validateOnlyDrink() {
+        List<String> drinkList = new ArrayList<>(List.of("제로콜라", "레드와인", "샴페인"));
+
+        for (String menu : menuList) {
+            if (!drinkList.contains(menu)) {
+                return false;
+            }
+        }
+        menuList.clear();
+        throw new IllegalArgumentException(ErrorMessages.NOT_ALLOWED_ONLY_DRINK.getErrorMsg());
+    }
+
+    public void validateDuplicatedMenu() {
+        Set<String> menuSet = new HashSet<>(menuList);
+
+        if (menuSet.size() != menuList.size()) {
+            menuList.clear();
+            throw new IllegalArgumentException(ErrorMessages.INCLUDE_DUPLICATED_MENU.getErrorMsg());
+        }
+    }
+
+    public void initiateOrderInfo() {
+        totalQuantity = 0;
+    }
+
+    public void validateMenuOrder(List<String> orderInfos) {
+        for (String order : orderInfos) {
+            isValidForm(order);
+        }
+
+        validateDuplicatedMenu();
+        validateOnlyDrink();
+    }
+
+    public int validateIsInteger(String string) {
+        int inputDate = 0;
+        try {
+            inputDate = Integer.parseInt(string);
+            totalQuantity += inputDate;
+        } catch (NumberFormatException e) {
+            errorMsg = ErrorMessages.NOT_NUMBER.getErrorMsg();
+            throw new IllegalArgumentException(errorMsg);
+        }
+
+        if (inputDate == 0) {
+            throw new IllegalArgumentException(ErrorMessages.NOT_ALLOW_ZERO.getErrorMsg());
+        }
+
+        return inputDate;
+    }
+
+    public int validateIsCorrectRange(int date) {
+        if (!((date >= 1) && (date <= 31))) {
+            errorMsg = ErrorMessages.INCORRECT_RANGE.getErrorMsg();
+            throw new IllegalArgumentException(errorMsg);
+        }
+
+        return date;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public void validateTotalOrderQuantity() {
+        if ((getTotalQuantity() < 1) || (getTotalQuantity() > 20)) {
+            errorMsg = ErrorMessages.NOT_INCLUDE_ORDER_RANGE.getErrorMsg();
+            totalQuantity = 0;
+            throw new IllegalArgumentException(errorMsg);
+        }
+    }
+
+    public void isExistMenu(String menuName) {
+        Menu menu = Menu.ROOT;
+        menu.contains(menuName);
+        menuList.add(menuName);
+    }
+}
