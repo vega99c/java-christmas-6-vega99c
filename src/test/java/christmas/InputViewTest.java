f2c86eaf91eead2ede9054d8eab244fcc5f11f87
vega <vega99c@naver.com>
2023-11-13 23:05:32 +0900

    test(InputViewTest): 메인메뉴,디저트 각 몇개씩 주문했는지 확인 테스트 추가
    - 할인 적용하기 위해서 미리 테스트 중

--- src/test/java/christmas/InputViewTest.java
+++ src/test/java/christmas/InputViewTest.java
@@ -3,12 +3,15 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class InputViewTest {
     @Test
     void 날짜입력값_숫자확인() {
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
         String inputString = "32";
 
         assertThat(restaurant.validateIsInteger(inputString)).isEqualTo(32);
@@ -16,7 +19,8 @@ public class InputViewTest {
 
     @Test
     void 날짜입력값_범위확인() {
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
         int inputDate = 32;
 
         assertThatThrownBy(() -> restaurant.validateIsCorrectRange(inputDate))
@@ -28,14 +32,17 @@ public class InputViewTest {
         String orderInfo = "해산물파스타-1";
         String menuName = orderInfo.split("-")[0];
 
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
+
         restaurant.isExistMenu(menuName);
     }
 
     @Test
     void 입력된주문양식_유효성확인() {
         String orderInfo = "해산물파스타12";
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
 
         assertThatThrownBy(() -> restaurant.isValidForm(orderInfo))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -45,7 +52,8 @@ public class InputViewTest {
     void 총_주문갯수확인() {
         String orderInfo1 = "해산물파스타-2";
         String orderInfo2 = "제로콜라-7";
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
 
         restaurant.isValidForm(orderInfo1);
         restaurant.isValidForm(orderInfo2);
@@ -57,7 +65,8 @@ public class InputViewTest {
     void 중복메뉴확인() {
         String orderInfo1 = "해산물파스타-2";
         String orderInfo2 = "해산물파스타-7";
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
 
         restaurant.isValidForm(orderInfo1);
 
@@ -70,12 +79,25 @@ public class InputViewTest {
         String orderInfo1 = "제로콜라-2";
         String orderInfo2 = "레드와인-3";
 
-        Restaurant restaurant = new Restaurant();
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
 
         restaurant.isValidForm(orderInfo1);
         restaurant.isValidForm(orderInfo2);
 
         assertThatThrownBy(() -> restaurant.validateOnlyDrink())
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메인메뉴_디저트_몇개주문했는지() {
+        List<String> orderInfo = new ArrayList<>(List.of("초코케이크-2", "해산물파스타-3"));
+
+        Customer customer = new Customer();
+        Restaurant restaurant = new Restaurant(customer);
+
+        restaurant.validateMenuOrder(orderInfo);
+        assertThat(customer.getDessertMenuCount()).isEqualTo(2);
+        assertThat(customer.getMainMenuCount()).isEqualTo(3);
     }
 }
