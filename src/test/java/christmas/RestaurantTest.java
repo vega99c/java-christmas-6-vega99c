0530187c5f423b2118b06f3e9c7f902baf5955d0
vega <vega99c@naver.com>
2023-11-13 16:54:12 +0900

    test(Restaurant): 증정 이벤트 해당 유무 확인 테스트 추가

--- src/test/java/christmas/RestaurantTest.java
+++ src/test/java/christmas/RestaurantTest.java
@@ -19,4 +19,17 @@ public class RestaurantTest {
 
         assertThat(restaurant.getTotalPrice()).isEqualTo(109000);
     }
+
+    @Test
+    void 증정이벤트_해당확인() {
+        List<String> orderList = new ArrayList<>(List.of("티본스테이크-4")); //55,000 + 54,000
+
+        Restaurant restaurant = new Restaurant();
+
+        restaurant.validateMenuOrder(orderList);
+        restaurant.calculateTotalPrice();
+
+        assertThat(restaurant.isHavingGifts()).isEqualTo(true);
+    }
+
 }
