409fffadba6d5c1b305c906aa9c9746b752d5bf6
vega99c <vega99c@naver.com>
2023-11-15 18:56:03 +0900

    입력된 날짜가 해당월의 유효일자인지 확인 테스트

--- src/test/java/christmas/EventPlanTest.java
+++ src/test/java/christmas/EventPlanTest.java
@@ -16,7 +16,14 @@ public class EventPlanTest {
     @Test
     void 월마지막날확인() {
         EventPlan plan = new EventPlan(2023, 12, 1);
-        
+
         assertThat(plan.getLastDay()).isEqualTo(31);
+    }
+
+    @Test
+    void 날짜_유효범위_검사() {
+        EventPlan plan = new EventPlan(2023, 12, 3);
+
+        assertThat(plan.validateIsCorrectRange(plan.getDayInt())).isEqualTo(true);
     }
 }
