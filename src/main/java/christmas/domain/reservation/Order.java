d481f300b4c1aa0b2cbe6cb140ba2820d341fb99
vega99c <vega99c@naver.com>
2024-01-05 14:21:25 +0900

    #12 Order모델에 필요없는 메서드 삭제

--- src/main/java/christmas/domain/reservation/Order.java
+++ src/main/java/christmas/domain/reservation/Order.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Order {
     List<OrderCompletedMenu> orders = new ArrayList<>();
 
-    // 입력된 주문 데이터를 가지고 OrderedMenu 리스트에 추가 로직
+    // 입력된 주문 데이터를 가지고 정상적인 주문들만 orders 리스트에 추가하는 로직
     public List<OrderCompletedMenu> ordersByInputData(String inputData) {
         List<String> orderStringList = new ArrayList<>(List.of(inputData.split(",")));
 
@@ -23,9 +23,5 @@ public class Order {
         OrderValidator.validateTotalOrderQuantity(orders);
 
         return orders;
-    }
-
-    public void printOrders() {
-
     }
 }
