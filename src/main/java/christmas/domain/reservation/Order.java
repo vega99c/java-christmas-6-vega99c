a7eb8cdd29b6e1c2517163c2060049503785b23d
vega99c <vega99c@naver.com>
2024-01-10 18:49:10 +0900

    #9 refactor: 이벤트 모델들 수정하면서 필요한 메서드 추가 작성
    - Order모델에서 다루는게 맞는지 고민했는데 우선은 여기에 작성하는 걸로 결정

--- src/main/java/christmas/domain/reservation/Order.java
+++ src/main/java/christmas/domain/reservation/Order.java
@@ -1,12 +1,18 @@
 package christmas.domain.reservation;
 
+import christmas.domain.event.Badge;
+import christmas.domain.event.Discount;
 import christmas.domain.menu.Menu;
 import christmas.validation.OrderValidator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
     List<OrderCompletedMenu> orders = new ArrayList<>();
+    VisitDate visitDate;
+    Discount discountEvent;
+    Badge badgeEvent;
 
     // 입력된 주문 데이터를 가지고 정상적인 주문들만 orders 리스트에 추가하는 로직
     public List<OrderCompletedMenu> ordersByInputData(String inputData) {
@@ -22,6 +28,44 @@ public class Order {
         OrderValidator.validateMenuOnlyDrink(orders);
         OrderValidator.validateTotalOrderQuantity(orders);
 
+        discountEvent.checkWholeDiscountEventPossible();
+        badgeEvent.checkBadgeEventPossible(getDiscountEvent().getTotalDiscountBenefit());
         return orders;
+    }
+
+    public void setVisitDate(VisitDate visitDate) {
+        this.visitDate = visitDate;
+        discountEvent = new Discount(visitDate, this);
+    }
+
+    public int getDessertMenuOrderCount() {
+        int dessertCount = 0;
+        for (OrderCompletedMenu menu : orders) {
+            if (Objects.equals(menu.getMenu().getMenuType(), "<디저트>")) {
+                dessertCount += menu.getQuantity();
+            }
+        }
+
+        return dessertCount;
+    }
+
+    public int getMainMenuOrderCount() {
+        int mainMenuCount = 0;
+        for (OrderCompletedMenu menu : orders) {
+
+            if (Objects.equals(menu.getMenu().getMenuType(), "<메인>")) {
+                mainMenuCount++;
+            }
+        }
+
+        return mainMenuCount;
+    }
+
+    public List<OrderCompletedMenu> getOrders() {
+        return orders;
+    }
+
+    public Discount getDiscountEvent() {
+        return discountEvent;
     }
 }
