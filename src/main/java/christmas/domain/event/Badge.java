bfad4682b37dcef9f5834032d6e07dd5452765ed
vega99c <vega99c@naver.com>
2024-01-10 19:00:00 +0900

    #9 refactor: 이벤트 뱃지 증정 관련 모델 업데이트
    - 총 할인금액에 따라 뱃지를 부여하는데 checkBadgeEventPossible메서드에 인자값을 안넣고 진행해보려했지만 현재 모델들의 연관상태로는 넣어야 하는걸로 결정. 좀 더 생각해보고 바뀔 수 있음

--- src/main/java/christmas/domain/event/Badge.java
+++ src/main/java/christmas/domain/event/Badge.java
@@ -1,27 +1,50 @@
 package christmas.domain.event;
 
+import com.sun.source.tree.WhileLoopTree;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Badge {
     SANTA_BADGE("산타", 20000),
     TREE_BADGE("트리", 10000),
-    STAR_BADGE("별", 5000);
+    STAR_BADGE("별", 5000),
+    NONE("없음", 0);
 
-    private int benefitCondition;
+    private int satisfiedBenefitCondition;
     private String badgeName;
+    private Badge badgeToGive;
 
     Badge(String name, int condition) {
         badgeName = name;
-        benefitCondition = condition;
+        satisfiedBenefitCondition = condition;
     }
 
-    public int getBenefitCondition() {
-        return benefitCondition;
+    public int getSatisfiedBenefitCondition() {
+        return satisfiedBenefitCondition;
     }
 
     public String getBadgeName() {
         return badgeName;
     }
 
-    public void checkBadgeEventPossible() {
+    public void checkBadgeEventPossible(int totalBenefit) {
+        List<Badge> wholeBadges = Arrays.stream(Badge.values()).toList();
 
+        for (Badge badge : wholeBadges) {
+            if (totalBenefit >= badge.getSatisfiedBenefitCondition()) {
+                setBadgeToGive(badge);
+                return;
+            }
+        }
+
+        setBadgeToGive(NONE);
+    }
+
+    public void setBadgeToGive(Badge badge) {
+        badgeToGive = badge;
+    }
+
+    public Badge getBadgeToGive() {
+        return badgeToGive;
     }
 }
