c325fc4155a2700de51906951ba0c8db8961c2a2
vega <vega99c@naver.com>
2024-01-11 14:37:25 +0900

    #9 이벤트 배지 모델 클래스로 변경 및 내부 enum 추가

--- src/main/java/christmas/domain/event/Badge.java
+++ src/main/java/christmas/domain/event/Badge.java
@@ -1,50 +1,52 @@
 package christmas.domain.event;
 
-import com.sun.source.tree.WhileLoopTree;
 import java.util.Arrays;
 import java.util.List;
 
-public enum Badge {
-    SANTA_BADGE("산타", 20000),
-    TREE_BADGE("트리", 10000),
-    STAR_BADGE("별", 5000),
-    NONE("없음", 0);
+public class Badge {
+    public enum BadgeType {
+        SANTA_BADGE("산타", 20000),
+        TREE_BADGE("트리", 10000),
+        STAR_BADGE("별", 5000),
+        NONE("없음", 0);
+        private int satisfiedBenefitCondition;
+        private String badgeName;
 
-    private int satisfiedBenefitCondition;
-    private String badgeName;
-    private Badge badgeToGive;
+        BadgeType(String name, int condition) {
+            badgeName = name;
+            satisfiedBenefitCondition = condition;
+        }
 
-    Badge(String name, int condition) {
-        badgeName = name;
-        satisfiedBenefitCondition = condition;
+        public String getBadgeName() {
+            return badgeName;
+        }
+
+        public int getSatisfiedBenefitCondition() {
+            return satisfiedBenefitCondition;
+        }
     }
 
-    public int getSatisfiedBenefitCondition() {
-        return satisfiedBenefitCondition;
-    }
+    private BadgeType badgeToGive;
 
-    public String getBadgeName() {
-        return badgeName;
-    }
 
     public void checkBadgeEventPossible(int totalBenefit) {
-        List<Badge> wholeBadges = Arrays.stream(Badge.values()).toList();
+        List<BadgeType> wholeBadges = Arrays.stream(BadgeType.values()).toList();
 
-        for (Badge badge : wholeBadges) {
+        for (BadgeType badge : wholeBadges) {
             if (totalBenefit >= badge.getSatisfiedBenefitCondition()) {
                 setBadgeToGive(badge);
                 return;
             }
         }
 
-        setBadgeToGive(NONE);
+        setBadgeToGive(BadgeType.NONE);
     }
 
-    public void setBadgeToGive(Badge badge) {
+    public void setBadgeToGive(BadgeType badge) {
         badgeToGive = badge;
     }
 
-    public Badge getBadgeToGive() {
+    public BadgeType getBadgeToGive() {
         return badgeToGive;
     }
 }
